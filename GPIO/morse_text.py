c4253009dc43fd5f464eff1aaceb8b635d1b143a
“klugook” <ryanreddy@hotmail.com>
2021-12-16 19:44:44 +0100

    working on morse_code and led WORKING

--- GPIO/morse_text.py
+++ GPIO/morse_text.py
@@ -6,32 +6,27 @@ GPIO.setwarnings( 0 )
 
 print( "GPIO morse text" )
 
+
 def pulse( pin_nr, high_time, low_time ):
-   """
-   Geef een puls op de pin:
-   Maak de pin pin_nr hoog, wacht high_time,
-   maak de pin laag, en wacht nog low_time
-   # copieer hier je pulse implementatie
+   GPIO.output(pin_nr, GPIO.HIGH )
+   time.sleep(high_time)
+   GPIO.output(pin_nr,GPIO.LOW)
+   time.sleep(low_time)
+
 
 def morse( pin_nr, dot_length, text ):
-   """
-   Laat de text horen als morse code.
-   De pin_nr is de pin die gebruikt wordt.
-   De text mag de volgende characters bevatten: spatie, streepje, punt.
-   De dot_length is de lengte van een punt (dot).
-   De lengte van de andere characters wordt daar van afgeleid.
-   """
-   # copieer hier je morse implementatie
+   for i in text:
+      if i == '.':
+         pulse(pin_nr, dot_length, dot_length)
+      if i == '-':
+         pulse(pin_nr, dot_length*3, dot_length)
+      if i == ' ':
+         time.sleep(dot_length*3)
+
 
 def morse_text( pin_nr, dot_length, text ):
-   """
-   Laat de string s horen als morse code.
-   De pin_nr is de pin die gebruikt wordt.
-   De text mag de volgende characters bevatten: lowercase letters, spatie.
-   De dot_length is de lengte van een punt (dot).
-   De lengte van de andere characters wordt daar van afgeleid.
-   """
-   # implementeer deze functie
+   morse(pin_nr, dot_length, text)
+
 
 led = 18
 GPIO.setup( led, GPIO.OUT )
