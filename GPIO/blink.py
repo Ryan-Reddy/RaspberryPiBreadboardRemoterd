ce4be537abf3ca0d5949864545fd18f3d7589c09
“klugook” <ryanreddy@hotmail.com>
2021-12-16 19:06:08 +0100

    0

--- GPIO/blink.py
+++ GPIO/blink.py
@@ -8,6 +8,6 @@ print( "GPIO blink" )
 GPIO.setup( 18, GPIO.OUT )
 while True:
    GPIO.output( 18, GPIO.HIGH )
-   time.sleep( 0.5 )
+   time.sleep( 0.03 )
    GPIO.output( 18, GPIO.LOW )
-   time.sleep( 0.5 )
+   time.sleep( 1.2 )
--- GPIO/morse_text.py
+++ GPIO/morse_text.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+
 GPIO.setmode( GPIO.BCM )
 GPIO.setwarnings( 0 )
 
