4d188d54cbe4f5c4d1f25a510f68d94cbfc90685
“klugook” <ryanreddy@hotmail.com>
2021-12-22 00:38:09 +0100

    working on input

--- input/copy.py
+++ input/copy.py
@@ -6,13 +6,19 @@ GPIO.setwarnings( 0 )
 print( "input copy" )
 
 led = 18
-switch = 23
+switch_on_23 = 23
+switch_off_24 = 24
 
 GPIO.setup( led, GPIO.OUT )
-GPIO.setup( switch, GPIO.IN, pull_up_down=GPIO.PUD_DOWN )
+GPIO.setup(switch_on_23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(switch_on_24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+
 while True:
-   if( GPIO.input( switch ) ):
+   if( GPIO.input(switch_on_23)):
       GPIO.output( led, GPIO.HIGH )
-   else:
+   if( GPIO.input(switch_on_24)):
       GPIO.output( led, GPIO.LOW )
+   # else:
+   #    GPIO.output( led, GPIO.LOW )
    time.sleep( 0.1 )
