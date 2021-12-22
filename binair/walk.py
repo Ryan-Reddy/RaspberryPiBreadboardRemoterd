2ac4eab11f344d2f49f7722ae7f135dc0dda9a46
“klugook” <ryanreddy@hotmail.com>
2021-12-22 12:00:58 +0100

    7

--- binair/walk.py
+++ binair/walk.py
@@ -3,9 +3,10 @@ import time
 GPIO.setmode( GPIO.BCM )
 GPIO.setwarnings( 0 )
 
+# ********************************************************************
 print( "binair walk 6" )
 
-led_pins = [ 18, 4, 17, 27, 22 ,10]
+led_pins = [ 18, 3, 17, 27, 22 ,10]
 
 for gpio in led_pins:
    GPIO.setup( gpio, GPIO.OUT )
@@ -19,15 +20,48 @@ def leds( pins, value, delay ):
         value = value // 2
     time.sleep( delay )
 
-delay = 0.2
+delay = 0.125
+def KITT(switch):
+    if switch == True:
+        while True:
+            leds( led_pins,  1, delay )
+            leds( led_pins,  2, delay )
+            leds( led_pins,  4, delay )
+            leds( led_pins,  8, delay )
+            leds( led_pins, 16, delay )
+            leds( led_pins, 32, delay )
+
+            leds( led_pins, 32, delay )
+            leds( led_pins, 16, delay )
+            leds( led_pins,  8, delay )
+            leds( led_pins,  4, delay )
+            leds( led_pins,  2, delay )
+            leds( led_pins,  1, delay )
+    if switch == False:
+        return
+
+# ********************************************************************
+# KITTSWITCH
+
+switch_on_23 = 23
+switch_off_24 = 24
+
+GPIO.setup(switch_on_23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(switch_off_24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
 while True:
-   leds( led_pins,  1, delay )
-   leds( led_pins,  2, delay )
-   leds( led_pins,  4, delay )
-   leds( led_pins,  8, delay )
-   leds( led_pins, 16, delay )
-   leds( led_pins, 32, delay )
+   if( GPIO.input(switch_on_23)):
+       switch = True
+       KITT(switch)
+
+   if( GPIO.input(switch_off_24)):
+       switch = False
+       KITT(switch)
+   time.sleep( 0.1 )
 
 
-zzz
-https://i.ytimg.com/vi/QmQRmZkUrVU/maxresdefault.jpg
+
+
+
+
+# https://i.ytimg.com/vi/QmQRmZkUrVU/maxresdefault.jpg
