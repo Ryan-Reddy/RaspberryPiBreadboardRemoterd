5d1c2d4ec3fcbc3dab2eb2775701e67de29b3038
“klugook” <ryanreddy@hotmail.com>
2021-12-22 12:23:01 +0100

    adding potentiometer to input

---
+++ input/potentiometer.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode( GPIO.BCM )
+GPIO.setwarnings( 0 )
+
+print( "input copy" )
+
+pot = 16
+
+GPIO.setup(switch_on_23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(switch_off_24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+
+while True:
+
+   if( GPIO.input(switch_on_23)):
+      GPIO.output( led, GPIO.HIGH )
+      GPIO.setup(switch_on_23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+   if( GPIO.input(switch_off_24)):
+      GPIO.output( led, GPIO.LOW )
+      GPIO.setup(switch_on_23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+
+   time.sleep( 0.1 )
